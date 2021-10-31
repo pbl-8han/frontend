dbd170d71b09f4b5db810f500f1284d2c4d24fe0
lockdef <locktki@icloud.com>
2021-10-31 15:42:57 +0900

    feat: 人数表示までのマークアップ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
+import { Text,  } from '@chakra-ui/react'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <>
-      <p>main page.</p>
+      <Header />
+      <Text as="p" fontSize="xl" align="center" marginTop={6}>
+        現在の人数は<Text as="big">N人</Text>です。
+      </Text>
     </>
   )
 }
